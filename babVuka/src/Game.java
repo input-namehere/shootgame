7f69fc6fef7d05609cbd767d92f0d5d7da3f3bae
kakhuno022 <kakhuno022@student.wethinkcode.co.za>
2023-05-27 14:01:59 +0200

    skeleton of the project

---
+++ babVuka/src/Game.java
@@ -0,0 +1,10 @@
+public class Game {
+    private String name;
+    private final int shieldStrength = 10; // Constant shield strength
+    private int bullets = 5 ;
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
--- babVuka/src/Player.java
+++ babVuka/src/Player.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Player {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Enter your name");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+
+        scanner.close();
     }
 }
